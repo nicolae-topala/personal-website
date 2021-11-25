8ba212c7424db781bb7c919e21c1f87ce96d10a2
nicolae.topala <nicolae.topala13@gmail.com>
2021-11-25 11:58:41 +0300

    added Icons for Navbar

--- src/ui/molecules/Navbar/Navbar.tsx
+++ src/ui/molecules/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import { history } from 'libs/history';
 import { useMenu } from 'hooks/useMenu';
 
 import './Navbar.scss';
+import { Icon } from 'resources/icons/icons';
+import env from '@beam-australia/react-env';
 
 export const Navbar = (): React.ReactElement => {
   const menu = useMenu();
@@ -16,7 +18,39 @@ export const Navbar = (): React.ReactElement => {
     <>
       <div className="navbar">
         <div className="navbar__title">Me</div>
-        <div className="navbar__socials">Social links</div>
+        <div className="navbar__socials">
+          <a
+            href={env('LINKEDIN_URL')}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="linkedin" />
+          </a>
+          <a href={env('GITHUB_URL')} target="_blank" rel="noopener noreferrer">
+            <Icon type="github" />
+          </a>
+          <a
+            href={env('INSTAGRAM_URL')}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="instagram" />
+          </a>
+          <a
+            href={env('FACEBOOK_URL')}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="facebook" />
+          </a>
+          <a
+            href={env('TWITTER_URL')}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon type="twitter" />
+          </a>
+        </div>
       </div>
 
       <div className="menu">
