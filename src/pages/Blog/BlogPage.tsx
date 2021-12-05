a6720424d27fbb32543e7d5c51d4055cd1e351ba
nicolae.topala <nicolae.topala13@gmail.com>
2021-12-05 14:12:11 +0300

    handling axios calls

--- src/pages/Blog/BlogPage.tsx
+++ src/pages/Blog/BlogPage.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import env from '@beam-australia/react-env';
-import { posts } from 'libs/http/posts/posts';
+import { posts as postsHttp } from 'libs/http/posts/posts';
+
+import { Posts } from 'libs/http/posts/posts.types';
+import { Tweet } from 'libs/http/posts/tweets.types';
 
 import { Layout } from 'ui/organisms/Layout/Layout';
 import { BigThumbnail } from './atoms/BigThumbnail/BigThumbnail';
@@ -10,85 +13,78 @@ import TwitterAvatar from 'pages/About/resources/1.jpg';
 import './BlogPage.scss';
 
 export const BlogPage = (): React.ReactElement => {
-  const getData = async () => {
-    const data = await posts.getPosts();
-    console.log(data);
-  };
+  const [posts, setPosts] = useState<Posts[]>();
+  const [tweets, setTweets] = useState<{ data: Tweet[] }>();
 
-  const getTweets = async () => {
-    const data = await posts.getTweets('228456219');
-    console.log(data);
-  };
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await postsHttp.getPosts();
 
-  getTweets();
+      setPosts(data);
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const getTweets = async () => {
+      const { data } = await postsHttp.getTweets('228456219');
+
+      setTweets(data);
+    };
+
+    getTweets();
+  }, []);
 
   return (
     <Layout>
       <div className="blog">
         <div className="blog__posts">
-          <BigThumbnail data={{ id: 1, coverUrl: '', title: 'First Post' }} />
-          <div className="blog__posts__posts-grid">
-            <SmallThumbnail
-              data={{ id: 2, coverUrl: '', title: 'Second Post' }}
-            />
-            <SmallThumbnail
-              data={{ id: 3, coverUrl: '', title: 'Third Post' }}
-            />
-            <SmallThumbnail
-              data={{ id: 4, coverUrl: '', title: 'Fourth Post' }}
-            />
-          </div>
+          {posts && posts.length !== 0 ? (
+            <>
+              <BigThumbnail data={posts[0]} />
+              <div className="blog__posts__posts-grid">
+                {posts.length > 1 &&
+                  posts
+                    .slice(1)
+                    .map((item) => (
+                      <SmallThumbnail key={item.id} data={item} />
+                    ))}
+              </div>
+            </>
+          ) : (
+            <p>Currently no posts</p>
+          )}
         </div>
         <div className="blog__twitter-posts">
           <div className="blog__twitter-posts__title">Recent Tweets</div>
           <div className="blog__twitter-posts__tweets">
-            <div className="post">
-              <a
-                href={env('TWITTER_URL')}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="header">
-                  <img
-                    src={TwitterAvatar}
-                    className="header__avatar"
-                    alt="My avatar"
-                  />
-                  <div className="header__name">Nick</div>
+            {tweets &&
+              tweets.data.slice(0, 5).map((item) => (
+                <div key={item.id} className="post">
+                  <a
+                    href={env('TWITTER_URL')}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="header">
+                      <img
+                        src={TwitterAvatar}
+                        className="header__avatar"
+                        alt="My avatar"
+                      />
+                      <div className="header__name">Nick</div>
+                    </div>
+                  </a>
+                  <a
+                    href={env('TWITTER_URL')}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="post-text">{item.text}</div>
+                  </a>
                 </div>
-              </a>
-              <a
-                href={env('TWITTER_URL')}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="post-text">This is a simple post</div>
-              </a>
-            </div>
-
-            <div className="post">
-              <a
-                href={env('TWITTER_URL')}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="header">
-                  <img
-                    src={TwitterAvatar}
-                    className="header__avatar"
-                    alt="My avatar"
-                  />
-                  <div className="header__name">Nick</div>
-                </div>
-              </a>
-              <a
-                href={env('TWITTER_URL')}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <div className="post-text">This is a simple post</div>
-              </a>
-            </div>
+              ))}
           </div>
         </div>
       </div>
