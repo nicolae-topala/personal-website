8cf1a82361a796e3beeac9c511355e388dcd81ac
nicolae.topala <nicolae.topala13@gmail.com>
2021-12-05 20:10:47 +0300

    API calls and fuctionality for BlogPostPage

--- src/pages/BlogPost/BlogPostPage.tsx
+++ src/pages/BlogPost/BlogPostPage.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import env from '@beam-australia/react-env';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { TwitterShareButton, FacebookShareButton } from 'react-share';
+
+import { Posts } from 'libs/http/posts/posts.types';
+import { posts as postsHttp } from 'libs/http/posts/posts';
 
 import { Layout } from 'ui/organisms/Layout/Layout';
 import { PostAction } from './atoms/PostAction/PostAction';
@@ -9,47 +12,78 @@ import { PostAction } from './atoms/PostAction/PostAction';
 import './BlogPostPage.scss';
 
 export const BlogPostPage: React.FC = (): React.ReactElement => {
+  const [post, setPost] = useState<Posts>();
+
   const location = useLocation();
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const getPost = async () => {
+      const { data } = await postsHttp.getPost(id);
+      console.log(data);
+      setPost(data);
+    };
+
+    getPost();
+  }, [id]);
 
   const onCopyLink = (): void => {
     navigator.clipboard.writeText(`${env('WEBSITE_URL')}${location.pathname}`);
   };
 
+  const getPostDate = (): string => {
+    if (post?.createdAt) {
+      const d = new Date(post.createdAt);
+
+      const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+      const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
+      const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+
+      return `${month} ${day}, ${year}`;
+    }
+
+    return '';
+  };
+
   return (
     <Layout>
       <div className="blog-post">
-        <img src="" className="blog-post__cover" alt="Blog Cover" />
-        <div className="blog-post__title">Title</div>
-        <div className="blog-post__date">Date</div>
-        <div className="blog-post__post">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-            sed mollis massa. Sed mollis hendrerit efficitur. Donec ante justo,
-            pulvinar ut nisl sit amet, congue convallis sem. Nam tempus lorem in
-            sodales viverra.
-          </p>
-        </div>
-        <div className="blog-post__post-actions">
-          <div className="blog-post__post-actions__socials">
-            <TwitterShareButton
-              url="http://twitter.com"
-              className="react-share"
-            >
-              <PostAction iconType="small-twitter" message="Tweet" />
-            </TwitterShareButton>
-            <FacebookShareButton
-              url="http://facebook.com"
-              className="react-share"
-            >
-              <PostAction iconType="small-facebook" message="Share" />
-            </FacebookShareButton>
-            <PostAction
-              iconType="copy"
-              message="Copy Link"
-              onClick={onCopyLink}
+        {post && (
+          <>
+            <img
+              src={post.coverUrl}
+              className="blog-post__cover"
+              alt="Blog Cover"
             />
-          </div>
-        </div>
+            <div className="blog-post__title">{post.title}</div>
+            <div className="blog-post__date">{getPostDate()}</div>
+            <div
+              className="blog-post__post"
+              dangerouslySetInnerHTML={{ __html: post.text }}
+            />
+            <div className="blog-post__post-actions">
+              <div className="blog-post__post-actions__socials">
+                <TwitterShareButton
+                  url={`${env('WEBSITE_URL')}${location.pathname}`}
+                  className="react-share"
+                >
+                  <PostAction iconType="small-twitter" message="Tweet" />
+                </TwitterShareButton>
+                <FacebookShareButton
+                  url={`${env('WEBSITE_URL')}${location.pathname}`}
+                  className="react-share"
+                >
+                  <PostAction iconType="small-facebook" message="Share" />
+                </FacebookShareButton>
+                <PostAction
+                  iconType="copy"
+                  message="Copy Link"
+                  onClick={onCopyLink}
+                />
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   );
