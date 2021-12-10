e9599cce134afcd296b3edfc7456189b5455ea43
nicolae.topala <nicolae.topala13@gmail.com>
2021-12-10 17:56:03 +0300

    Axios-Token Configuration

--- src/libs/http/axios.ts
+++ src/libs/http/axios.ts
@@ -1,6 +1,20 @@
 import $axios from 'axios';
 import env from '@beam-australia/react-env';
+import { load } from 'react-cookies';
 
 export const axios = $axios.create();
 
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 axios.defaults.baseURL = env('API_URL');
+
+export const axiosHeadersUpdater = (): void =>{
+    const token = load('token');
+
+    if (token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    } else if (axios.defaults.headers.common.Authorization){
+        delete axios.defaults.headers.common.Authorization;
+    }
+}
+
+axiosHeadersUpdater();
